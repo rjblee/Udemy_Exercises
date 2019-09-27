ae3602a6af1d0bbdcd49b85744bcd877030c25c9
Ralph <ralph.lee@live.ca>
2019-09-27 20:30:50 +0000

    jQuery Selector exercise

---
+++ 209.js
@@ -0,0 +1,11 @@
+//Select al ldivs and give them a purple background
+$("div").css("background", "purple");
+
+//Select the divs with class "highlight" and make them 200 px width
+$("div.highlight").css("width", "200px");
+
+//Select the div with id "third" and give it an orange border
+$("div#third").css("border", "3px solid orange");
+
+//Select the first div only and change its font color to pink
+$("div:first-of-type").css("color", "pink");
