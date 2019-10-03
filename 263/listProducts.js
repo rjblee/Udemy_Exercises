bd0b11ce4547215034ecb5acd3eb05bb46122cfc
Ralph <ralph.lee@live.ca>
2019-10-03 19:57:18 +0000

    Install a npm package and exercise

---
+++ 263/listProducts.js
@@ -0,0 +1,17 @@
+var fakeData = require('faker');
+
+// console.log(fakeData.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}"));4
+// console.log(fakeData.internet.email());
+
+console.log("-------------------")
+console.log("WELCOME TO MY SHOP")
+console.log("-------------------")
+
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
+console.log(fakeData.commerce.productName() + " - $" + fakeData.commerce.price());
