f230640e760294df59217ca8a3d36af56d18ef2f
Ralph <ralph.lee@live.ca>
2019-10-03 03:05:07 +0000

    Node average calculator exercise

---
+++ 258.js
@@ -0,0 +1,17 @@
+function average(array) {
+  var sum = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  var avg = sum/array.length;
+
+  return Math.round(avg);
+};
+
+var scores = [90, 98, 89, 100, 100, 86, 94];
+console.log(average(scores));
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+console.log(average(scores2));
