e97e2c26b06353b67f328703d48e0277ce3437c0
Ralph <ralph.lee@live.ca>
2019-10-10 21:28:56 +0000

    Add results.ejs with a list of movie titles

--- 286/app.js
+++ 286/app.js
@@ -2,11 +2,13 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
+
 app.get("/results", function(req, res){
-  request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+  request("http://omdbapi.com/?s=seoul&apikey=thewdb", function(error, response, body){
     if(!error && response.statusCode == 200){
-      var results = JSON.parse(body);
-      res.send(results["Search"][0]);
+      var data = JSON.parse(body);
+      res.render("results", {data: data});
     }
   })
 });
