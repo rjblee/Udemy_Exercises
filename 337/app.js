1f56c3ab3202706bf395e9cf2a134beb3c3cdb75
Ralph <ralph.lee@live.ca>
2019-10-23 06:54:08 +0000

    Add home.ejs

--- 337/app.js
+++ 337/app.js
@@ -1,1 +1,17 @@
-var express = require("express");
+var express            = require("express");
+var app                = express();
+
+
+app.set('view engine', 'ejs');
+
+
+app.get("/", function(req, res){
+  res.render("home");
+})
+
+
+
+
+app.listen(3000, function(){
+  console.log("Server started............!");
+})
